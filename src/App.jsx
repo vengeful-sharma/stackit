17bb32c21a4387e0bbce366cfec2f616cde40724
vengeful-sharma <surajkumar4officials@gmail.com>
2025-07-12 12:48:09 +0530

    correct talwind version

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>
+      <h1 className='text-xl text-red-500'>
         Hello world!
       </h1>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-],
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
